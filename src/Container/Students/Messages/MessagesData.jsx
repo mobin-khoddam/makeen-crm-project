7616184474befcd2ac911ab418aa241ed435e1aa
Mohammad <mohammadsoltani138@gmail.com>
2024-11-05 10:12:28 +0330

    ui of messages is completed

---
+++ src/Container/Students/Messages/MessagesData.jsx
@@ -0,0 +1,56 @@
+export const MessageData = [
+  {
+    id: 1,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+  {
+    id: 2,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+  {
+    id: 3,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+  {
+    id: 4,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+  {
+    id: 5,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+  {
+    id: 6,
+    title: "اعلام تعطیلی",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه ",
+    date: "1402.03.12",
+    image:
+      "https://s3-alpha-sig.figma.com/img/d6b7/24b5/7a885710560ef025b6040d370c5e7f2e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aZyNoM9mOyf-bySGcUdo1Su7wsRYgMIii1DBGx3PFCITXIjGj2Sm8KeNA4nz-2N0kYA39HaAHDqvz8hAQ2HUNxdfsc9PzU2wDcU~GIHyJuGFT7o24iPCYcmiBS26kiAEjYMHJJjlOr-ADUru3hM1mzy8a2IcfUwawX9Z~RHm38azceIsQvjeZb5pEJMfeZ1xDN3Lze6yWUHFTglSuYg2iElk3gAxZXVNHKBfkfUdvqcp-uAQst5s7vrdgWVXL5dlUOLvZvBP~rX7ENPaYqS3Dtg5mS-HJ1MFBR5~ivP-jAbRNgp5mArj9kGw2eohH6EMapkUJQ~GeX9ycYPwPZSCxQ__",
+  },
+];
--- src/Container/Students/WorkReport/WorkReportBar/SeeWorkReportsPage/CategoriesFilter/CategoriesList.jsx
+++ src/Container/Students/WorkReport/WorkReportBar/SeeWorkReportsPage/CategoriesFilter/CategoriesList.jsx
@@ -1,21 +1,23 @@
-import {CategoriesFilterList} from "../../../../../../helper/ProjectData/Data.js";
+import { CategoriesFilterList } from "../../../../../../helper/ProjectData/Data.js";
 
-const CategoriesList = ({setFilter, setShowCategories}) => {
-    const setFilterHandler = (item)=> {
-        setShowCategories(false);
-        setFilter(item)
-    }
-    return (
-        <div className="bg-white w-[234px] rounded-lg mt-4 p-2">
-            {
-                CategoriesFilterList.map((item, index) => (
-                    <div onClick={()=>setFilterHandler(item)} className="pr-3 py-2 border border-white rounded-md hover:border-primary-blue " key={index}>
-                        {item}
-                    </div>
-                ))
-            }
+const CategoriesList = ({ setFilter, setShowCategories }) => {
+  const setFilterHandler = (item) => {
+    setShowCategories(false);
+    setFilter(item);
+  };
+  return (
+    <div className="bg-white w-[234px] rounded-lg mt-4 p-2">
+      {CategoriesFilterList.map((item, index) => (
+        <div
+          onClick={() => setFilterHandler(item)}
+          className="pr-3 py-2 border border-white rounded-md hover:border-primary-blue "
+          key={index}
+        >
+          {item}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default CategoriesList;
