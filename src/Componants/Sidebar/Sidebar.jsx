fbcf52903f045ce976070c00709e5c9300c464b2
mobin <dota.is.me.20@gmail.com>
2024-11-14 19:38:47 +0330

    side bar fixed

--- src/Componants/Sidebar/Sidebar.jsx
+++ src/Componants/Sidebar/Sidebar.jsx
@@ -1,13 +1,6 @@
-import homeSvg from "../../assets/images/Sidebar/home.svg";
-import noteSvg from "/src/assets/images/Sidebar/note.svg";
-import financeSvg from "/src/assets/images/Sidebar/card-send.svg";
-import LeaveRequestsSvg from "/src/assets/images/Sidebar/cake.svg";
-import MessagesSvg from "/src/assets/images/Sidebar/sms.svg";
-import PersonalSvg from "/src/assets/images/Sidebar/profile.svg";
-import regulationsSvg from "/src/assets/images/Sidebar/judge.svg";
 import logOutIcon from "/src/assets/images/Sidebar/logOut.png";
-import CustomSidebarLink from "../../helper/CustomSidebarLink/CustomSidebarLink.jsx";
 import MakeenLogo from "../MakeenLogo/MakeenLogo.jsx";
+import SideBarNavLinks from "./SideBarNavLinks.jsx";
 const Sidebar = () => {
   return (
     <div className="">
@@ -21,45 +14,12 @@ const Sidebar = () => {
             className="drawer-overlay"
           ></label>
           <div className=" bg-white text-base-content w-[240px] rounded-2xl py-6 pl-0">
+
             <div className="mb-6 mx-auto w-fit">
               <MakeenLogo />
             </div>
             <div className="w-full pr-6 flex flex-col gap-6 mb-44">
-              <CustomSidebarLink
-                location={"/student/dashboard"}
-                svg={`${homeSvg}`}
-                title="داشبورد"
-              />
-              <CustomSidebarLink
-                location={"/student/work-report"}
-                svg={`${noteSvg}`}
-                title="گزارش کار"
-              />
-              <CustomSidebarLink
-                location={"/student/finance"}
-                svg={`${financeSvg}`}
-                title="مالی"
-              />
-              <CustomSidebarLink
-                location={"/student/leave-request"}
-                svg={`${LeaveRequestsSvg}`}
-                title="درخواست های مرخصی"
-              />
-              <CustomSidebarLink
-                location={"/student/messages"}
-                svg={`${MessagesSvg}`}
-                title="پیام ها"
-              />
-              <CustomSidebarLink
-                location={"/student/personal-information"}
-                svg={`${PersonalSvg}`}
-                title="اطلاعات فردی"
-              />
-              <CustomSidebarLink
-                location={"/student/asd"}
-                svg={`${regulationsSvg}`}
-                title="آیین نامه انضباطی"
-              />
+            <SideBarNavLinks />
             </div>
             <div className="flex gap-4 mb-6 cursor-pointer pr-6">
               <img className="w-6 h-6" src={`${logOutIcon}`} alt="" />
@@ -67,6 +27,7 @@ const Sidebar = () => {
                 خروج از حساب کاربری
               </span>
             </div>
+
           </div>
         </div>
       </div>
