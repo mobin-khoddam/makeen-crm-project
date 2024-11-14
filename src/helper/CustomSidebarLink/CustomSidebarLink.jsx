65cf531446f83e1a551766e819c41531dd213250
Mohammad <mohammadsoltani138@gmail.com>
2024-11-14 15:57:57 +0330

    update sidebar

--- src/helper/CustomSidebarLink/CustomSidebarLink.jsx
+++ src/helper/CustomSidebarLink/CustomSidebarLink.jsx
@@ -1,18 +1,34 @@
-import {Link} from "react-router-dom";
-import {useLocation} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
+const CustomSidebarLink = ({ svg, location, title }) => {
+  const thisLocation = useLocation();
 
-const CustomSidebarLink = ({svg, location, title}) => {
-    const thisLocation = useLocation();
-    const isActive = (path) => thisLocation.pathname.split('/')[1] === path.split('/')[1];
-    return (
-        <Link className={`flex items-center gap-4 font-semibold w-full ${isActive(location) && 'border-l-4 border-primary-blue'}`} to={location}>
-                <div className={`w-6 h-6 ${isActive(location) ? 'bg-primary-blue' : 'bg-[#5C5353]'}`}
-                     style={{mask: `url(${svg}) no-repeat center`}}>
-                </div>
-            <span className={`${isActive(location) ? 'text-primary-blue' : 'text-[#5C5353]'}`}>{title}</span>
-        </Link>
-    )
-}
+  const isActive = (path) => {
+    return thisLocation.pathname === path;
+  };
+  return (
+    <Link
+      className={`flex items-center gap-4 font-semibold w-full ${
+        isActive(location) && "border-l-4 border-primary-blue"
+      }`}
+      to={location}
+    >
+      <div
+        className={`w-6 h-6 ${
+          isActive(location) ? "bg-primary-blue" : "bg-[#5C5353]"
+        }`}
+        style={{ mask: `url(${svg}) no-repeat center` }}
+      ></div>
+      <span
+        className={`${
+          isActive(location) ? "text-primary-blue" : "text-[#5C5353]"
+        }`}
+      >
+        {title}
+      </span>
+    </Link>
+  );
+};
 
-export default CustomSidebarLink
+export default CustomSidebarLink;
