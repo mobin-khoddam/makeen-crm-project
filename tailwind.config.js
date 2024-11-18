5cceffcee09dc1ed2e49a33254eae1771d83661d
mobin <dota.is.me.20@gmail.com>
2024-11-18 12:39:36 +0330

    add package tailwind scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import Index from 'tailwind-scrollbar'
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -13,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [daisyui],
+  plugins: [daisyui, Index],
 };
