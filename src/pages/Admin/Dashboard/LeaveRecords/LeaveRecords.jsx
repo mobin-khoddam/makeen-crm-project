fdf8a82fa53de9eca898adaf0e4414fe0a6ca976
mobin <dota.is.me.20@gmail.com>
2024-11-23 17:01:03 +0330

    selectorr fixed and date picker position

--- src/pages/Admin/Dashboard/LeaveRecords/LeaveRecords.jsx
+++ src/pages/Admin/Dashboard/LeaveRecords/LeaveRecords.jsx
@@ -6,9 +6,9 @@ import {useState} from "react";
 import DatePicker from "../../../../Componants/Datepicker/DatePicker.jsx";
 
 const LeaveRecords = () => {
-    const [state, setState] = useState()
-    const [state1, setState1] = useState()
-    const [state2, setState2] = useState()
+    const [state, setState] = useState(false)
+    const [state1, setState1] = useState(false)
+    const [state2, setState2] = useState(false)
     const data = [
         'فرانت اند',
         'بک اند',
@@ -23,19 +23,26 @@ const LeaveRecords = () => {
         'python',
     ]
 
+    const closeHandler = () => {
+        state && setState(false)
+        state1 && setState1(false)
+        state2 && setState2(false)
+    }
 
     return (
-        <OriginalTemplatePage>
-            <div className='z-20 flex gap-8'>
-                <CategoriesFilter showCategories={state} setShowCategories={setState} className='text-primary-blue' title='نام اصلی دوره' data={data} />
-                <CategoriesFilter showCategories={state1} setShowCategories={setState1} className='text-primary-blue' title='نام فرعی دوره' data={data2} />
-                <CategoriesFilter showCategories={state2} setShowCategories={setState2} className='text-primary-blue' title='شماره دوره' data={data3} />
-                <DatePicker />
-            </div>
-            <div className='z-10'>
-                <LeaveRecordsTable tableBody={adminPanelLeaveRecords.tableBody} tableHead={adminPanelLeaveRecords.tableHead} />
-            </div>
-        </OriginalTemplatePage>
+        <div onClick={closeHandler}>
+            <OriginalTemplatePage>
+                <div className=' grid grid-cols-4 max-xl:grid-cols-2 max-sm:grid-cols-1 max-xl:items-center max-xl:w-full w-fit gap-8 mb-10'>
+                    <CategoriesFilter showCategories={state} setShowCategories={setState} className='text-primary-blue' title='نام اصلی دوره' data={data} />
+                    <CategoriesFilter showCategories={state1} setShowCategories={setState1} className='text-primary-blue' title='نام فرعی دوره' data={data2} />
+                    <CategoriesFilter showCategories={state2} setShowCategories={setState2} className='text-primary-blue' title='شماره دوره' data={data3} />
+                    <DatePicker />
+                </div>
+                <div>
+                    <LeaveRecordsTable tableBody={adminPanelLeaveRecords.tableBody} tableHead={adminPanelLeaveRecords.tableHead} />
+                </div>
+            </OriginalTemplatePage>
+        </div>
     )
 }
 export default LeaveRecords
