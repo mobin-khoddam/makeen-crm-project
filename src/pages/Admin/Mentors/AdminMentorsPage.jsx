5a6fc86feb796a429930ba5081501e781f81e476
Mohammad <mohammadsoltani138@gmail.com>
2024-11-23 16:52:18 +0330

    mentors in admin is almost done

---
+++ src/pages/Admin/Mentors/AdminMentorsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OriginalTemplatePage from "../../../Componants/OriginalTemplate/OriginalTemplatePage";
+import AdminMentors from "../../../Container/Admin/Mentors/AdminMentors";
+
+const AdminMentorsPage = () => {
+  return (
+    <OriginalTemplatePage>
+      <AdminMentors />
+    </OriginalTemplatePage>
+  );
+};
+
+export default AdminMentorsPage;
